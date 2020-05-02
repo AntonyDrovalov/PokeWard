1258f0f2c91b61a3a4af4fe74254c6c31b2b25be
AntonyDrovalov <anton@drovalev.pp.ua>
2020-05-02 23:12:24 +0300

    added login/registration screens (tested on local PC with txt files)

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class Login : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    private string Username;
+    private string Password;
+    private String[] Lines;
+    private string DecryptedPass;
+
+    public void toRegisterButton()
+    {
+        Application.LoadLevel("RegisterScreen");
+    }
+
+    public void LoginButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        if(Username != "")
+        {
+            if(System.IO.File.Exists(@"C:/Anton/UnityTest/" + Username + ".txt")) //api check
+            {
+                UN = true;
+                Lines = System.IO.File.ReadAllLines(@"C:/Anton/UnityTest/" + Username + ".txt"); //api get login and pass 
+            }
+            else
+            {
+                Debug.LogWarning("Username Invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username Field Empty");
+        }
+        if(Password != "")
+        {
+            if (System.IO.File.Exists(@"C:/Anton/UnityTest/" + Username + ".txt"))
+            {
+                int i = 1;
+                foreach (char c in Lines[1])
+                {
+                    i++;
+                    char Decrypted = (char)(c / i);
+                    DecryptedPass += Decrypted.ToString();
+                }
+                if (Password == DecryptedPass)
+                {
+                    PW = true;
+                }
+                else
+                {
+                    Debug.LogWarning("Password is Invalid");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Password is Invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password Field Empty");
+        }
+        if (UN == true && PW == true)
+        {
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            print("Login Sucsessful");
+            Application.LoadLevel("SampleScene");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Username != "" && Password != "")
+            {
+                LoginButton();
+            }
+        }
+
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+
+    }
+}
---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class Register : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    public GameObject confPassword;
+    private string Username;
+    private string Password;
+    private string ConfPassword;
+    private string form;
+
+    public void toLoginButton()
+    {
+        Application.LoadLevel("LoginScreen");
+    }
+
+    public void RegisterButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        bool CPW = false;
+
+        if (Username != "") {
+            if (!System.IO.File.Exists(@"C:/Anton/UnityTest/" + Username + ".txt")) //api check
+            {
+                UN = true;
+            }
+            else
+            {
+                Debug.LogWarning("Username Taken");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username field Empty");
+        }
+
+        if (Password != "")
+        {
+            if (Password.Length > 5)
+            {
+                PW = true;
+            }
+            else
+            {
+                Debug.LogWarning("Password Must Be atleast 6 Characters long");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password Field Empty");
+        }
+
+        if (ConfPassword != "")
+        {
+            if (ConfPassword == Password)
+            {
+                CPW = true;
+            }
+            else
+            {
+                Debug.LogWarning("Passwords Dont match");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Confirm Password Field Empty");
+        }
+        
+        if(UN == true && PW == true && CPW == true)
+        {
+            bool Clear = true;
+            int i = 1;
+            foreach(char c in Password)
+            {
+                if(Clear)
+                {
+                    Password = "";
+                    Clear = false;
+                }
+                i++;
+                char Encrypted = (char)(c * i);
+                Password += Encrypted.ToString();
+            }
+            form = (Username + Environment.NewLine + Password);
+            System.IO.File.WriteAllText(@"C:/Anton/UnityTest/" + Username + ".txt", form); // api send req
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            confPassword.GetComponent<InputField>().text = "";
+            print("Registration Complete");
+            Application.LoadLevel("LoginScreen");
+        }
+
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confPassword.GetComponent<InputField>().Select();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Username != "" && Password != "" && ConfPassword != "")
+            {
+                RegisterButton();
+            }
+        }
+
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfPassword = confPassword.GetComponent<InputField>().text;
+    }
+}
