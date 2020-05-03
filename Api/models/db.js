356fc4d2e426e5a2841f9c56566ada1b543f05b4
ZheniaG04 <evhenguts@gmail.com>
2020-05-03 16:31:37 +0300

    fixed connection issue

--- Api/models/db.js
+++ Api/models/db.js
@@ -2,18 +2,23 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  port: dbConfig.PORT,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+var connection;
+connectDB();
 
 // open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
+  if (error) throw connectDB();
   console.log("Successfully connected to the database.");
 });
 
 module.exports = connection;
+
+function connectDB(){
+  connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    port: dbConfig.PORT,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+  });
+}
